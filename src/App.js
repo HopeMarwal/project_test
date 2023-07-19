64bee22a470db97b062e557b29c8c8b9940dcdcd
HopeMarwal <vremea.dr@gmail.com>
2023-07-19 13:25:49 +0300

    db

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import './assets/styles/global.scss';
-import { useEffect } from 'react';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
 function App() {
-  useEffect(() => {
-    const dataFetch = async () => {
-      const data = await (
-        await fetch(
-          "https://my-json-server.typicode.com/HopeMarwal/project_test/posts"
-        )
-      ).json();
-
-      // set state when the data received
-      console.log(data);
-    };
-
-    dataFetch();
-  }, [])
-
-
   return (
     <div className="App">
       <Nav />
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Nav() {
+    useEffect(() => {
+        const dataFetch = async () => {
+          const data = await (
+            await fetch(
+              "https://my-json-server.typicode.com/HopeMarwal/project_test/nav"
+            )
+          ).json();
+    
+          // set state when the data received
+          console.log(data);
+        };
+    
+        dataFetch();
+      }, [])
   return (
     <nav>Nav</nav>
   )
